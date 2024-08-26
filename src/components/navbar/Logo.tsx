9be316e893c4583e1835a17f16490bda5ef42a77
arielferen <arieliten85@gmail.com>
2024-08-26 15:33:03 -0300

    feat: add new styles navbar component

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -2,6 +2,6 @@ import camera from "../../assets/icono-camera.png";
 export const Logo = () => (
   <div className="logo">
     <img src={camera} alt="Logo" />
-    <p data-test="logo">Laura Lopez</p>
+    <p>Laura Lopez</p>
   </div>
 );
