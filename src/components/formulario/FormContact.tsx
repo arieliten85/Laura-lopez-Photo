aafa1fd1b2007dfa6f1bd010910c1f08d9987c7f
arielferen <arieliten85@gmail.com>
2024-09-06 17:10:14 -0300

    feat: error build css styles

--- src/components/formulario/FormContact.tsx
+++ src/components/formulario/FormContact.tsx
@@ -1,6 +1,6 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import "./FormContact.css";
+import "./formContact.css";
 
 export const FormContact = () => {
   // Esquema de validación con Yup
